c8a08eb360294afc27770101f02d9c64ffd84a5f
Awsproject-Rama <96468243+Awsproject-Rama@users.noreply.github.com>
2021-12-22 06:28:00 +0000

    Update el.tf

--- AWS_Project/PART1_Scripts/Terraform/PublicSubnet/el.tf
+++ AWS_Project/PART1_Scripts/Terraform/PublicSubnet/el.tf
@@ -23,22 +23,35 @@ resource "aws_security_group" "elb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-## Creating ELB
-resource "aws_elb" "ELB" {
-  name = "terraform-asg-instance"
-  subnets = [data.aws_cloudformation_export.PublicSubnetID1.value,data.aws_cloudformation_export.PublicSubnetID2.value ]
-  #availability_zones = ["${data.aws_availability_zones.all.names}"]
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:8080/"
+## Creating NLB
+
+  resource "aws_lb_listener" "listener" {
+    load_balancer_arn = "${aws_lb.NLB.arn}"
+    port              = "8080"
+    protocol          = "TCP"
+    default_action {
+      target_group_arn = "${aws_lb_target_group.lbtg.arn}"
+      type             = "forward"
+    }
   }
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "8080"
-    instance_protocol = "http"
+  resource "aws_lb_target_group" "lbtg" {
+    name     = "terraform-elb-tg"
+    port     = "22"
+    protocol = "TCP"
+    vpc_id = data.aws_cloudformation_export.VpcID.value
+    target_type = "instance"
+    deregistration_delay = "100"
+    health_check {
+      interval = "30"
+      port = "22"
+      protocol = "TCP"
+      healthy_threshold = "2" 
+      unhealthy_threshold= "2" 
+    }
   }
-}
+  resource "aws_lb" "NLB" {
+    name = "terraform-asg-instance"
+    load_balancer_type = "network"
+    subnets = [data.aws_cloudformation_export.PublicSubnetID1.value,data.aws_cloudformation_export.PublicSubnetID2.value ]
+    enable_cross_zone_load_balancing = true
+  }
